f1a33d9e9d3a287d1a0da6faefea6559cf8086bd
Umar Khan <37391937+Umar24129@users.noreply.github.com>
2022-01-30 12:25:32 +0100

    new_UI_files_minor_Code

--- UserInterface/main.cpp
+++ UserInterface/main.cpp
@@ -1,11 +1,12 @@
-#include "kms.h"
-#include <QApplication>
+#include "widget.h"
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    kms k;
-    kms w;
+    QApplication a (argc, argv);
+    Widget w;
+    w.resize(400,300);
     w.show();
     return a.exec();
 }
